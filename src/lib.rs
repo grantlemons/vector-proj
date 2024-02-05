2ad9ad1470af6b1248d53d89a74538b741439d55
Grant Lemons <grantlemons@aol.com>
2024-02-04 22:08:53 -0700

    add some tests

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,12 @@ impl std::fmt::Display for Vector {
     }
 }
 
+impl PartialEq for Vector {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y && self.z == other.z
+    }
+}
+
 impl std::ops::Mul<f32> for &Vector {
     type Output = Vector;
 
@@ -28,14 +34,26 @@ impl std::ops::Mul<f32> for &Vector {
     }
 }
 
-impl std::ops::Add<Vector> for &Vector {
+impl std::ops::Add<&Vector> for &Vector {
     type Output = Vector;
 
-    fn add(self, rhs: Vector) -> Self::Output {
+    fn add(self, rhs: &Vector) -> Self::Output {
         Vector {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::Neg for Vector {
+    type Output = Vector;
+
+    fn neg(self) -> Self::Output {
+        Vector {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
         }
     }
 }
@@ -51,8 +69,12 @@ impl From<(f32, f32, f32)> for Vector {
 }
 
 impl Vector {
-    pub fn new(x: f32, y: f32, z: f32) -> Self {
-        Vector { x, y, z }
+    pub fn new<T: Into<f32>>(x: T, y: T, z: T) -> Self {
+        Vector {
+            x: x.into(),
+            y: y.into(),
+            z: z.into(),
+        }
     }
 
     pub fn rotate_x(mut self, angle: f32) {
@@ -83,12 +105,8 @@ impl Vector {
         f32::sqrt(f32::powi(self.x, 2) + f32::powi(self.y, 2) + f32::powi(self.z, 2))
     }
 
-    pub fn cross_product(&self, rhs: &Self) -> Self {
-        Vector {
-            x: (self.y * rhs.z) - (self.z * rhs.y),
-            y: (self.z * rhs.x) - (self.x * rhs.z),
-            z: (self.x * rhs.y) - (self.y * rhs.x),
-        }
+    pub fn unit(&self) -> Vector {
+        self * (1_f32 / self.magnitude())
     }
 
     pub fn proj(lhs: &Self, rhs: &Self) -> Self {
@@ -99,11 +117,58 @@ impl Vector {
         (self.x * rhs.x) + (self.y * rhs.y) + (self.z * rhs.z)
     }
 
+    pub fn cross_product(&self, rhs: &Self) -> Self {
+        Vector {
+            x: (self.y * rhs.z) - (self.z * rhs.y),
+            y: (self.z * rhs.x) - (self.x * rhs.z),
+            z: (self.x * rhs.y) - (self.y * rhs.x),
+        }
+    }
+
     pub fn proj_onto(&self, rhs: &Self) -> Self {
         Self::proj(rhs, self)
     }
 
     pub fn angle(&self, rhs: &Self) -> f32 {
         f32::acos(Vector::dot_product(self, rhs) / (self.magnitude() * self.magnitude()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn unit_magnitude() {
+        let i_unit_vector: Vector = Vector::new(1_f32, 0_f32, 0_f32);
+        let j_unit_vector: Vector = Vector::new(0_f32, 1_f32, 0_f32);
+        let k_unit_vector: Vector = Vector::new(0_f32, 0_f32, 1_f32);
+
+        assert_eq!(i_unit_vector.magnitude(), 1_f32);
+        assert_eq!(j_unit_vector.magnitude(), 1_f32);
+        assert_eq!(k_unit_vector.magnitude(), 1_f32);
+    }
+
+    #[test]
+    fn unit_cross_product() {
+        let i_unit_vector: Vector = Vector::new(1_f32, 0_f32, 0_f32);
+        let j_unit_vector: Vector = Vector::new(0_f32, 1_f32, 0_f32);
+        let k_unit_vector: Vector = Vector::new(0_f32, 0_f32, 1_f32);
+
+        assert_eq!(i_unit_vector.cross_product(&j_unit_vector), k_unit_vector);
+        assert_eq!(j_unit_vector.cross_product(&k_unit_vector), i_unit_vector);
+        assert_eq!(k_unit_vector.cross_product(&i_unit_vector), j_unit_vector);
+
+        assert_eq!(
+            j_unit_vector.cross_product(&j_unit_vector),
+            -(j_unit_vector.cross_product(&j_unit_vector))
+        );
+    }
+
+    #[test]
+    fn unit_unit_scalar() {
+        let i_unit_vector: Vector = Vector::new(1_f32, 0_f32, 0_f32);
+
+        assert_eq!((&i_unit_vector * 2_f32).unit(), i_unit_vector)
     }
 }
