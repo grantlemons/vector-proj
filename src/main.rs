e0bce1e3bbf666525ca4c60430c2c2ef7788e0a6
Grant Lemons <grantlemons@aol.com>
2024-02-05 16:27:26 -0700

    change to _f32 in main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use vector_lib::*;
 
 pub fn main() {
-    let u = Vector::new(2.0, 4.0, 6.0);
-    let v = Vector::new(3.0, 4.0, 5.0);
+    let u = Vector::new(-8_f32, -2_f32, 20_f32);
+    let v = Vector::new(-6_f32, -12_f32, 36_f32);
 
     println!("Dot Product: {}", u.dot_product(&v));
 
     println!("Cross Product (u x v): {}", u.cross_product(&v));
     println!("Cross Product (v x u): {}", v.cross_product(&u));
 
-    println!("u Scaled to 2.0x: {}", &u * 2.0);
-    println!("u Scaled to 1/2: {}", &u * (1.0 / 2.0));
+    println!("u Scaled to 2x: {}", &u * 2_f32);
+    println!("u Scaled to 1/2x: {}", &u * (1_f32 / 2_f32));
 
     println!("u projected onto v: {}", u.proj_onto(&v));
     println!("v projected onto u: {}", v.proj_onto(&u));
